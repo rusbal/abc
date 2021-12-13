341b1123bcb015becd581b86ee0934ffa9a32433
Raymond <raymond@philippinedev.com>
2021-12-13 18:19:05 +0800

    initial commit

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@ git pull
 docker-compose stop web
 docker-compose up -d
 docker exec -it abc_web_1 bin/bundle install
+docker exec -it abc_web_1 bin/bundle db:create
 docker exec -it abc_web_1 bin/rails db:migrate
