d5cec6210bb3a79bda7c0421f7e3c4fd5a49b136
Raymond <raymond@philippinedev.com>
2021-12-13 19:25:44 +0800

    webpacker install

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,4 @@ docker-compose up -d
 docker exec -it abc_web_1 gem install bundler -v '~> 2.2'
 docker exec -it abc_web_1 bin/bundle install
 docker exec -it abc_web_1 bin/rails db:migrate
+docker exec -it abc_web_1 rails webpacker:install
