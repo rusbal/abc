f80741c6b1195a4e27c935e2c1a67d2e103dc140
Raymond <raymond@philippinedev.com>
2021-12-13 19:03:43 +0800

    shopify app

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 git pull
 docker-compose stop web
 docker-compose up -d
+docker exec -it abc_web_1 gem install bundler -v '~> 2.2'
 docker exec -it abc_web_1 bin/bundle install
 docker exec -it abc_web_1 bin/rails db:migrate
